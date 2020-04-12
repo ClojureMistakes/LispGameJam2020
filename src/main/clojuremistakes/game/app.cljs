2671c06386d7f141824590ece580c60a6935cbab
Steven Myers <larkenx@gmail.com>
2020-04-12 02:20:40 -0400

    trying and failing to create a generic game loop that moves, resizes, draws circles from randomly placed circles

--- src/main/clojuremistakes/game/app.cljs
+++ src/main/clojuremistakes/game/app.cljs
@@ -24,20 +24,7 @@
                         :container (pixi-utils/create-container app)
                         :total-elapsed-time 0})))
 
-(defn draw-circle [circle]
-  (let [{:keys [x
-                y
-                radius
-                borderWidth
-                borderColor
-                borderAlpha
-                backgroundColor]} (merge default-circle circle)
-        {:keys [graphics
-                container]} @state]
-    (.lineStyle graphics borderWidth borderColor borderAlpha)
-    (.beginFill graphics backgroundColor)
-    (.drawCircle graphics x y radius)
-    (.endFill graphics)))
+
 
 ; (defn game-loop [game]
 ;   (let []
@@ -54,37 +41,56 @@
   (println (:app @state))
   (.add (.-ticker (:app @state)) on-tick))
 
-(defonce my-persistent-circle (atom {:x (/ width 2)
-                                     :y (/ height 2)
+(defonce my-persistent-circle (atom {:x 10
+                                     :y 10
                                      :radius 10
                                      :backgroundColor 0x87ceeb}))
 
-(defn move-circle [delta]
+(defonce my-list-of-circles (list my-list-of-circles))
+
+(defn add-circle-to-list [circles]
+  (cons (atom (assoc default-circle :x (rand-int width) :y (rand-int height))) circles))
+
+(defn draw-circle [circle]
+  (let [{:keys [x
+                y
+                radius
+                borderWidth
+                borderColor
+                borderAlpha
+                backgroundColor]} (merge default-circle @circle)
+        {:keys [graphics]} @state]
+    ; (println (str "Drawing circle " @circle))
+    (.lineStyle graphics borderWidth borderColor borderAlpha)
+    (.beginFill graphics backgroundColor)
+    (.drawCircle graphics x y radius)
+    (.endFill graphics)))
+
+(defn resize-circle [delta circle]
   (let [{:keys [total-elapsed-time]} @state
         elapsed-time-in-seconds (Math/round (/ total-elapsed-time 100))]
-    (println elapsed-time-in-seconds)
-    (swap! my-persistent-circle assoc
+    (swap! circle assoc
            :radius (cond
                      (= 0 (mod elapsed-time-in-seconds 7)) (:radius default-circle)
-                     :else (+ (/ delta 10) (:radius @my-persistent-circle)))
+                     :else (+ (/ delta 10) (:radius @circle)))
            :borderWidth (cond
                           (= 0 (mod elapsed-time-in-seconds 7)) (:borderWidth default-circle)
-                          :else (+ (/ delta 10) (:borderWidth @my-persistent-circle)))
-           :x (cond
-                (= 0 (mod elapsed-time-in-seconds 7)) (:x default-circle)
-                (even? elapsed-time-in-seconds) (+ delta (:x @my-persistent-circle))
-                (= 0 (mod elapsed-time-in-seconds 3)) (+ (* 3 (rand-int delta)) (:x @my-persistent-circle))
-                :else (- (:x @my-persistent-circle) delta))
-           :y (cond
-                (= 0 (mod elapsed-time-in-seconds 7)) (:y default-circle)
-                (odd? elapsed-time-in-seconds) (+ delta (:y @my-persistent-circle))
-                (= 0 (mod elapsed-time-in-seconds 3)) (+ (* 3 (rand-int delta)) (:y @my-persistent-circle))
-                :else (- (:y @my-persistent-circle) delta)))))
+                          :else (+ (/ delta 30) (:borderWidth @circle))))))
 
-(defn render-game [delta]
-  (draw-circle @my-persistent-circle)
-;   (println (str "Drawing circle at " (:x @my-persistent-circle) "," (:y @my-persistent-circle)))
-  )
+(defn move-circle [delta circle]
+  (let [{:keys [total-elapsed-time]} @state
+        elapsed-time-in-seconds (Math/round (/ total-elapsed-time 100))]
+    (swap! circle assoc
+           :x (cond
+                ; (= 0 (mod elapsed-time-in-seconds 7)) (:x default-circle)
+                (even? elapsed-time-in-seconds) (+ delta (:x @circle))
+                (= 0 (mod elapsed-time-in-seconds 3)) (+ (* 3 (rand-int delta)) (:x @circle))
+                :else (- (:x @circle) delta))
+           :y (cond
+                ; (= 0 (mod elapsed-time-in-seconds 7)) (:y default-circle)
+                (odd? elapsed-time-in-seconds) (+ delta (:y @circle))
+                (= 0 (mod elapsed-time-in-seconds 3)) (+ (* 3 (rand-int delta)) (:y @circle))
+                :else (- (:y @circle) delta)))))
 
 (defn init []
   (let [{:keys [graphics
@@ -93,9 +99,12 @@
     (println "Adding shared grapics object to PIXI.Stage")
     (.addChild (.-stage app) graphics)
     (println "Initializing game loop")
+    (add-circle-to-list my-list-of-circles)
     (init-game-loop
      (fn [delta]
        (.clear (:graphics @state))
-       (move-circle delta)
-       (render-game delta)
+       (doseq [circle  (add-circle-to-list  (add-circle-to-list  (add-circle-to-list  (add-circle-to-list my-list-of-circles))))]
+         (move-circle delta circle)
+         (resize-circle delta circle)
+         (draw-circle circle))
        (swap! state assoc :total-elapsed-time (+ delta (:total-elapsed-time @state)))))))
